731f0e6d60594e96b2fb8c194e29643bff1e52ed
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-26 15:06:48 +0800

    fix:添加特定平台的android /ios代码

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,28 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller = window?.rootViewController as! FlutterViewController;
+        let channel = FlutterMethodChannel(name: "samples.flutter.dev/battery",
+                                                binaryMessenger: controller.binaryMessenger);
+        channel.setMethodCallHandler { (call, result) in
+            if call.method == "getBatteryLevel" {
+                self.receiveBatteryLevel(result: result)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    private func receiveBatteryLevel(result: FlutterResult) {
+      let device = UIDevice.current
+      device.isBatteryMonitoringEnabled = true
+      if device.batteryState == UIDevice.BatteryState.unknown {
+        result(FlutterError(code: "UNAVAILABLE",
+                            message: "Battery level not available.",
+                            details: nil))
+      } else {
+        result(Int(device.batteryLevel * 100))
+      }
+    }
 }
